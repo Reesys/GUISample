3b359249c0ca000dcc08799bc35fc58e46dc0437
Reesys <emailjoshuareyes@gmail.com>
2018-11-27 12:53:33 -0700

    Added in the import for the JLabel, JButton, etc. and added o the setupListener class byt aking an actionPerformed Listener and a background color change.

--- src/gui/view/SamplePanel.java
+++ src/gui/view/SamplePanel.java
@@ -1,5 +1,11 @@
 package gui.view;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import gui.controller.GUIController;
@@ -7,6 +13,8 @@ import gui.controller.GUIController;
 public class SamplePanel extends JPanel
 {
 	private GUIController appController;
+	private JLabel textLabel;
+	private JButton colorButton;
 
 	public SamplePanel(GUIController appController)
 	{
@@ -14,5 +22,46 @@ public class SamplePanel extends JPanel
 
 		this.appController = appController;
 
+		this.textLabel = new JLabel("This is a color app!");
+		this.colorButton = new JButton("Click here to change color");
+
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+
+	private void setupPanel()
+	{
+		this.setBackground(Color.CYAN);
+		this.add(textLabel);
+		this.add(colorButton);
+	}
+
+	private void setupLayout()
+	{
+
+	}
+
+	private void setupListeners()
+	{
+		colorButton.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent mouseClick)
+			{
+				//System.out.println("You clicked the button!!!!");
+				changeBackgroundColor();
+			}
+		});
+	}
+
+	private void changeBackgroundColor()
+	{
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+
+		this.setBackground(new Color(red, green, blue));
+		textLabel.setText("Red: " + red + " Green: " + green + " Blue: " + blue);
 	}
 }
