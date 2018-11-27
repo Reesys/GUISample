52afd20abf38eeabbad06900ee2626ce9b113c1f
Reesys <emailjoshuareyes@gmail.com>
2018-11-27 12:22:09 -0700

    Added to the GUIController by giving it a construtor and settng SampleFrame to this.

--- src/gui/controller/GUIController.java
+++ src/gui/controller/GUIController.java
@@ -1,7 +1,16 @@
 package gui.controller;
 
+import gui.view.SampleFrame;
+
 public class GUIController
 {
+	private SampleFrame appFrame;
+
+	public GUIController()
+	{
+		appFrame = new SampleFrame(this);
+	}
+
 	public void start()
 	{
 
